15c4e3f0d1cad014ab7515ad4fd4928c08228e85
Jacob A. Bruinsma <me@jacobbruinsma.com>
2021-04-04 08:42:41 -0500

    brush > tool, load/save, now writes tiff of a set 1k x 1k size

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #define HEIGHT 1024
 #define CHANNELS 3
 #define BDEPTH unsigned short
-#define BRUSHSIZE 31
+#define TOOLSIZE 31
 #define PI 3.141592653589793
 
 struct Pixel {
@@ -18,10 +18,13 @@ struct Pixel {
 // Alternatively, this:
 // https://stackoverflow.com/questions/22945647/why-does-a-
 //		large-local-array-crash-my-program-but-a-global-one-doesnt
+char header[0x5310];
+char footer[0x2D];
+long int footerAddress = 0x605310;
 BDEPTH image[WIDTH * HEIGHT * CHANNELS];
-BDEPTH brush[BRUSHSIZE * BRUSHSIZE * CHANNELS];
+BDEPTH tool[TOOLSIZE * TOOLSIZE * CHANNELS];
 
-void getImage(char *name, BDEPTH *img, size_t size)
+void load(char *name, BDEPTH *img, size_t size)
 {
 	FILE *ptr;
 	ptr = fopen(name, "rb");  // r for read, b for binary
@@ -29,11 +32,19 @@ void getImage(char *name, BDEPTH *img, size_t size)
 	fclose(ptr);
 }
 
-void setImage(char *name, BDEPTH *img, size_t size)
+void save(char *name, BDEPTH *img, size_t size)
 {
 	FILE *ptr;
-	ptr = fopen(name, "wb");  // r for read, b for binary
+	ptr = fopen("1024x1024x16b.tif", "rb");  // r for read, b for binary
+	fread(header, sizeof(header), 1, ptr);
+	fseek(ptr, footerAddress, SEEK_SET);
+	fread(footer, sizeof(footer), 1, ptr);
+	fclose(ptr);
+
+	ptr = fopen(name, "wb");  // w for write, b for binary
+	fwrite(header, sizeof(header), 1, ptr);
 	fwrite(img, size, 1, ptr);
+	fwrite(footer, sizeof(footer), 1, ptr);
 	fclose(ptr);
 }
 
@@ -51,70 +62,71 @@ void setPixel(BDEPTH *img, BDEPTH w, BDEPTH x, BDEPTH y, struct Pixel *p)
 	img[(CHANNELS * w * y) + (CHANNELS * x) + 2] = p->b;
 }
 
-BDEPTH min(BDEPTH a, BDEPTH b)
+BDEPTH min(unsigned int a, unsigned int b)
 {
 	return a < b ? a : b;
 }
 
-void paint(BDEPTH *img, BDEPTH *brush, BDEPTH x, BDEPTH y)
+void cut(BDEPTH *img, BDEPTH *tool, BDEPTH x, BDEPTH y, BDEPTH depth)
 {
-	BDEPTH brushX, brushY; 
+	BDEPTH toolX, toolY; 
 	int imageX, imageY;
 	struct Pixel pImage = {.r = 0, .g = 0, .b = 0};
-	struct Pixel pBrush = {.r = 0, .g = 0, .b = 0};
+	struct Pixel pTool = {.r = 0, .g = 0, .b = 0};
 	struct Pixel pFinal = {.r = 0, .g = 0, .b = 0};
-	for (brushY = 0; brushY < BRUSHSIZE; brushY++)
+	for (toolY = 0; toolY < TOOLSIZE; toolY++)
 	{
-		for (brushX = 0; brushX < BRUSHSIZE; brushX++)
+		for (toolX = 0; toolX < TOOLSIZE; toolX++)
 		{
-			imageX = x - ((BRUSHSIZE - 1) / 2) + brushX;
-			imageY = y - ((BRUSHSIZE - 1) / 2) + brushY;
+			imageX = x - ((TOOLSIZE - 1) / 2) + toolX;
+			imageY = y - ((TOOLSIZE - 1) / 2) + toolY;
 
 			if (imageX < 0) continue;
 			if (imageX >= WIDTH) continue;
 			if (imageY < 0) continue;
 			if (imageY >= HEIGHT) continue;
 
-			getPixel(brush, BRUSHSIZE, brushX, brushY, &pBrush);
+			getPixel(tool, TOOLSIZE, toolX, toolY, &pTool);
 			getPixel(img, WIDTH, imageX, imageY, &pImage);
-			pFinal.r = min(pBrush.r, pImage.r);
-			pFinal.g = min(pBrush.g, pImage.g);
-			pFinal.b = min(pBrush.b, pImage.b);
+			pFinal.r = (BDEPTH) min(pTool.r + depth, pImage.r);
+			pFinal.g = (BDEPTH) min(pTool.g + depth, pImage.g);
+			pFinal.b = (BDEPTH) min(pTool.b + depth, pImage.b);
 			setPixel(img, WIDTH, imageX, imageY, &pFinal);
 		}
 	}
 }
 
-void paintFloat(BDEPTH *img, BDEPTH *brush, float x, float y)
+void cutFloat(BDEPTH *img, BDEPTH *tool, float x, float y, BDEPTH depth)
 {
 	BDEPTH halfX = (BDEPTH) (WIDTH / 2); // 512
 	BDEPTH halfY = (BDEPTH) (HEIGHT / 2);
 	BDEPTH imageX = halfX + (BDEPTH) (x * (float) WIDTH / 2); // 512 + (1 * 1024 / 2) or 512 + (-1 * 1024 / 2)
 	BDEPTH imageY = HEIGHT - (halfY + (BDEPTH) (y * (float) HEIGHT / 2));
-	paint(img, brush, imageX, imageY);
+	cut(img, tool, imageX, imageY, depth);
 }
 
 int main() 
 {
 	size_t imgSize = sizeof(image);
-	size_t brsSize = sizeof(brush);
-	getImage("1024x1024x16b copy.raw", image, imgSize);
-	getImage("hardGradient.raw", brush, brsSize);
+	size_t brsSize = sizeof(tool);
+	load("1024x1024x16b.raw", image, imgSize);
+	load("hardGradient.raw", tool, brsSize);
 
 	int i;
-	int steps = 1000;
+	int steps = 10000;
 	double angle;
 	double twoPi = PI * 2;
 	double step = twoPi / steps;
 	float loopX, loopY;
+	BDEPTH depth = 32757;
 	for (i = 0; i < steps; i++)
 	{
 		angle = twoPi / steps * i;
 		loopX = (float) cos(angle);
 		loopY = (float) sin(angle);
-		paintFloat(image, brush, loopX, loopY);
+		cutFloat(image, tool, loopX, loopY, depth);
 	}
 
-	setImage("out.raw", image, imgSize);
+	save("out.tif", image, imgSize);
 	printf("done\n");
 }
